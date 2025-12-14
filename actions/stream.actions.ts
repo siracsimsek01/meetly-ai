ee22b1b4b31dcd436fb1e4fc11006c97d6a61df0
Sirac Simsek <88107978+siracsimsek01@users.noreply.github.com>
2025-12-14 13:46:58 +0000

    feat: add pnpm workspace configuration and implement Clerk middleware for route protection

--- actions/stream.actions.ts
+++ actions/stream.actions.ts
@@ -1,15 +1,15 @@
 'use server';
 
-import { currentUser } from "@clerk/nextjs/server";
+import { auth } from "@clerk/nextjs/server";
 import { StreamClient } from "@stream-io/node-sdk";
 
 const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 const apiSecret = process.env.STREAM_SECRET_KEY;
 
 export const tokenProvider = async () => {
-    const user = await currentUser();
+    const { userId } = await auth();
 
-    if(!user) throw new Error('User is not logged in');
+    if(!userId) throw new Error('User is not logged in');
     if(!apiKey) throw new Error('No API key');
     if(!apiSecret) throw new Error('No API secret');
 
@@ -18,7 +18,7 @@ export const tokenProvider = async () => {
     const exp = Math.round(new Date().getTime() / 1000) + 60 * 60;
     const issued = Math.floor(Date.now() / 1000) - 60;
 
-    const token = client.createToken(user.id, exp, issued);
+    const token = client.createToken(userId, exp, issued);
 
     return token;
 }
